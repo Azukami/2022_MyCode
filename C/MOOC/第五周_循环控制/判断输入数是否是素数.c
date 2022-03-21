b98f1fce7c6d544e37ad93fbe12985b69289af52
明日香米 <1751626462@qq.com>
2022-03-21 22:29:17 +0800

    个人练习

--- C/MOOC/第五周_循环控制/判断输入数是否是素数.c
+++ C/MOOC/第五周_循环控制/判断输入数是否是素数.c
@@ -18,7 +18,6 @@ int main()
             isPrime = 0;//num可以被除1和本身以外的数字整除，则num不是素数，此时让isPrime变为0表示num不是素数
             break;//使得isPrime为0时退出循环，无需做剩下的运算
         }
-        
     }
     if ( isPrime == 1 ) {
         printf("%d是素数\n", num);
